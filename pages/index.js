189826db35e7234d4a9c1530a50a9e6110cbcaae
SamuelsApps <154631883+SamuelsDev441@users.noreply.github.com>
2024-05-20 17:21:17 -0400

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,22 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  var MyClass = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Student Home</title>
         <h1>Student</h1>
-    </head>
-    <body>
-        <form action="/portal">
-            <label for="username">Username:</label><br>
-            <input type="text" id="username" name="user"><br>
-            <label for="pwd">Password:</label><br>
-            <input type="password" id="pwd" name="pwd">
-          </form>
-    </body>
-    </html>
+        <form>
+          <label htmlFor="username">Username:</label><br />
+          <input type="text" id="username" name="user" /><br />
+          <label htmlFor="pwd">Password:</label><br />
+          <input type="password" id="pwd" name="pwd" />
+        </form>
+      </div>
+    );
+  }
+});
   )
-}
