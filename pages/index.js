d64ea167c7616086693488c34953ebfbd9b8e169
SamuelsApps <154631883+SamuelsDev441@users.noreply.github.com>
2024-06-02 17:45:02 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import BibleVerse from '../components/BibleVerse';
 
 export default function Home() {
-  var MyClass = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Student Home</title>
-        <h1>Student</h1>
-        <form>
-          <label htmlFor="username">Username:</label><br />
-          <input type="text" id="username" name="user" /><br />
-          <label htmlFor="pwd">Password:</label><br />
-          <input type="password" id="pwd" name="pwd" />
-        </form>
-      </div>
-    );
-  }
-});
+  return (
+    <div>
+      <Head>
+        <title>Bible App</title>
+        <meta name="description" content="A simple Bible app made with Next.js" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <BibleVerse />
+      </main>
+    </div>
+  );
 }
